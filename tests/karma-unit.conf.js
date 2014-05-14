82d686b046db34af316880c8513a963a8b0c4409
Laurent Margirier <laurent.margirier@gmail.com>
2014-05-14 07:53:50 +0200

    tests: delete dependency to uto.require

--- tests/karma-unit.conf.js
+++ tests/karma-unit.conf.js
@@ -7,7 +7,6 @@ module.exports = function(config) {
       "bower_components/angular-animate/angular-animate.min.js",
       "bower_components/angular-sanitize/angular-sanitize.min.js",
       "bower_components/angular-ui-router/release/angular-ui-router.min.js",
-      "bower_components/angular-webkit-require/dist/app.js",
       "bower_components/flex/dist/ng-flexbox.js",
       "bower_components/angular-loading-bar/build/loading-bar.min.js",
       "bower_components/lodash/dist/lodash.min.js",
